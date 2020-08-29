8eca4da1dc53788a5be9cfa7265c3bf74651791d
Trevor Blades <tdblades@gmail.com>
2020-08-28 18:32:45 -0700

    Add page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Flex, Heading} from '@chakra-ui/core';
+import {Helmet} from 'react-helmet';
 
 export default function Index() {
   return (
-    <Flex as="header" align="center" px="6" h="16">
-      <Heading fontSize="2xl">DevHub</Heading>
-    </Flex>
+    <>
+      <Helmet title="DevHub" />
+      <Flex as="header" align="center" px="6" h="16">
+        <Heading fontSize="2xl">DevHub</Heading>
+      </Flex>
+    </>
   );
 }
