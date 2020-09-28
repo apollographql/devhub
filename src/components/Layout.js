90d7920c6f238dc28cce19ecfca10b413d45513b
Trevor Blades <tdblades@gmail.com>
2020-09-28 13:26:48 -0700

    Make bg dots responsive

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,30 +3,29 @@ import Header from './Header';
 import PropTypes from 'prop-types';
 import React from 'react';
 import grid from '../assets/grid.png';
-import {Box, Global} from '@chakra-ui/core';
+import {Box} from '@chakra-ui/core';
 import {Helmet} from 'react-helmet';
 
 export default function Layout({children}) {
   return (
-    <>
+    <Box
+      backgroundImage={`url(${grid})`}
+      backgroundPosition={{
+        base: 'right -360px top',
+        lg: 'right -180px top',
+        xl: 'right top'
+      }}
+      backgroundRepeat="no-repeat"
+    >
       <Helmet defaultTitle="DevHub" titleTemplate="%s - DevHub">
         <link rel="icon" href="https://www.apollographql.com/favicon.ico" />
       </Helmet>
-      <Global
-        styles={{
-          body: {
-            backgroundImage: `url(${grid})`,
-            backgroundPosition: 'top right',
-            backgroundRepeat: 'no-repeat'
-          }
-        }}
-      />
       <Header />
       <Box pt="8" pb="20" as="main">
         {children}
       </Box>
       <Footer />
-    </>
+    </Box>
   );
 }
 
