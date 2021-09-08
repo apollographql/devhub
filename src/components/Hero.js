5f07e01738fae10eec76b38381ad5c40808faec2
Janessa Garrow <janessa@apollographql.com>
2021-09-08 11:20:33 -0600

    Restrict hero content width

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import commandModule from '../assets/command-module.svg';
-import {Box, Flex, Heading, Img, Text} from '@chakra-ui/core';
+import {Box, Center, Flex, Heading, Img, Text} from '@chakra-ui/core';
 
 export default function Hero({title, description}) {
   return (
@@ -12,34 +12,34 @@ export default function Hero({title, description}) {
       align="center"
       mb="8"
     >
-      <Flex
+      <Center
         p="40px 44px"
         borderRadius={{base: '0', lg: '12px'}}
         bg="linear-gradient(360deg, #0A061E 8.24%, #311C87 100.52%)"
         color="white"
         w="full"
         maxW="1392px"
-        align="center"
-        justify="space-between"
       >
-        <Box>
-          <Text fontSize="xs" textTransform="uppercase">
-            GraphQL Resources
-          </Text>
-          <Heading my="2" as="h1" fontSize="2rem">
-            {title}
-          </Heading>
-          <Text w="370px" display="inline-block">
-            An Apollo data graph helps you build apps faster with less code.{' '}
-            {description}
-          </Text>
-        </Box>
-        <Img
-          src={commandModule}
-          alt="Space command module"
-          display={{base: 'none', lg: 'block'}}
-        />
-      </Flex>
+        <Flex align="center" justify="space-between" w="full" maxW="1152px">
+          <Box>
+            <Text fontSize="xs" textTransform="uppercase">
+              GraphQL Resources
+            </Text>
+            <Heading my="2" as="h1" fontSize="2rem">
+              {title}
+            </Heading>
+            <Text w="370px" display="inline-block">
+              An Apollo data graph helps you build apps faster with less code.{' '}
+              {description}
+            </Text>
+          </Box>
+          <Img
+            src={commandModule}
+            alt="Space command module"
+            display={{base: 'none', lg: 'block'}}
+          />
+        </Flex>
+      </Center>
     </Flex>
   );
 }
