c88839387a27d8c97a53370b71ab9ef9d4fb213b
Janessa Garrow <janessa@apollographql.com>
2021-09-15 14:33:14 -0600

    Add color fill hover animation to Resources cards

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -1,6 +1,14 @@
 import PropTypes from 'prop-types';
-import React from 'react';
-import {Box, Center, Grid, Heading, Text} from '@chakra-ui/core';
+import React, {useState} from 'react';
+import useDimensions from '../hooks/useDimensions';
+import {
+  Box,
+  Center,
+  Grid,
+  Heading,
+  Text,
+  useBreakpointValue
+} from '@chakra-ui/core';
 import {IconCalendarTime} from '@apollo/space-kit/icons/IconCalendarTime';
 import {IconComment} from '@apollo/space-kit/icons/IconComment';
 import {IconDocument} from '@apollo/space-kit/icons/IconDocument';
@@ -76,8 +84,47 @@ export default function Resources() {
   );
 }
 
+const ICON_WRAPPER_DIMENSIONS = {
+  w: '44px',
+  h: '44px',
+  borderRadius: '4px'
+};
+
+const COLOR_FILL_ANIMATIONS = {
+  transition: 'all 0.3 ease-out',
+  transform: 'translateZ(0)',
+
+  '.overlay': {
+    transform: 'scale(8) translateZ(0)'
+  },
+
+  '.icon-wrapper:after': {
+    background: 'blilet.50'
+  },
+
+  '.text': {
+    color: '#4c5656'
+  }
+};
 function Card({resource}) {
+  const cardHeight = useBreakpointValue({
+    base: '76px',
+    lg: '183px',
+    xl: '207px'
+  });
+  const [cardRef, {width}] = useDimensions();
+  const [scale, setScale] = useState({});
   const {href, icon, title, description} = resource;
+
+  React.useEffect(() => {
+    if (cardHeight) {
+      const cardHght = Number(cardHeight.slice(0, -2));
+      const scaleX = Math.ceil(width / 44) + 1;
+      const scaleY = Math.ceil(cardHght / 44) + 1;
+
+      setScale({x: scaleX, y: scaleY});
+    }
+  }, [width, cardHeight]);
 
   return (
     <Box
@@ -86,10 +133,12 @@ function Card({resource}) {
       borderColor="gray.200"
       borderRadius="8px"
       w="full"
-      h={{base: '76px', lg: '183px', xl: '207px'}}
+      h={cardHeight}
+      overflow="hidden"
     >
       <Box
         as="a"
+        ref={cardRef}
         href={href}
         p={{base: '4', lg: '6'}}
         w="full"
@@ -99,24 +148,73 @@ function Card({resource}) {
         gridGap="4"
         alignItems="center"
         justifyContent="flex-start"
+        pos="relative"
+        _hover={{
+          ...COLOR_FILL_ANIMATIONS,
+          '.overlay': {
+            transform: `scale(${scale.x}, ${scale.y}) translateZ(0)`
+          }
+        }}
+        _active={COLOR_FILL_ANIMATIONS}
+        _focus={COLOR_FILL_ANIMATIONS}
       >
-        <Center
-          p="3"
+        <Box
+          className="overlay"
+          {...ICON_WRAPPER_DIMENSIONS}
+          position="absolute"
           bg="blilet.50"
-          borderRadius="4px"
-          w="44px"
-          h="44px"
+          top={{base: '18px', lg: '25px'}}
+          left={{base: '18px', lg: '25px'}}
+          z-index="0"
+          transition="transform 0.3s ease-out"
+          transformOrigin="10% 30%" // TODO: find better values than just guess and check
+        />
+        <Center
+          className="icon-wrapper"
+          {...ICON_WRAPPER_DIMENSIONS}
+          w="46px"
+          h="46px"
+          p="3"
           mb={{base: '0', lg: '3'}}
+          bg="white"
+          borderWidth="1px"
+          borderColor="transparent"
+          pos="relative"
+          zIndex="1"
+          transition="all 0.3s ease-out"
+          _after={{
+            ...ICON_WRAPPER_DIMENSIONS,
+            content: "''",
+            display: 'block',
+            position: 'absolute',
+            background: 'blilet.50',
+            borderRadius: '3px',
+            top: '0',
+            left: '0',
+            transition: 'opacity 0.3s ease-out'
+          }}
         >
-          <Box as={icon} color="indigo.600" />
+          <Box
+            as={icon}
+            color="indigo.600"
+            zIndex="2"
+            transform="translateZ(0)"
+          />
         </Center>
-        <Heading as="h3" fontWeight="600" fontSize="1.125rem" mb="1">
-          {title}
-        </Heading>
-        <Text display={{base: 'none', lg: 'block'}}>{description}</Text>
-        <Center w="20px" h="20px" p="1" display={{lg: 'none'}}>
-          <Box as={IconProceed} w="full" h="full" />
-        </Center>
+        <Box
+          className="text"
+          zIndex="2"
+          pos="relative"
+          transition="color 0.3s ease-out"
+        >
+          <Heading as="h3" fontWeight="600" fontSize="1.125rem" mb="1">
+            {title}
+          </Heading>
+          <Text display={{base: 'none', lg: 'block'}}>{description}</Text>
+          <Center w="20px" h="20px" p="1" display={{lg: 'none'}}>
+            <Box as={IconProceed} w="full" h="full" />
+          </Center>
+        </Box>
       </Box>
     </Box>
   );
